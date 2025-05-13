63a10b88bc6d5386006894015f773f8cd47626f9
PrahaldChouhan <prahladchouhan928@gmail.com>
2025-05-13 17:05:13 +0530

    userState

--- React-one/src/App.jsx
+++ React-one/src/App.jsx
@@ -1,19 +1,43 @@
 
+import { use } from 'react';
 import { useState } from 'react';
 
 function App() {
-  const [username,setUsername]=useState("prahlad");
+  const [username,setUsername]=useState([
+    {name:'john',age:19},
+    {name:'ankur',age:32},
+    {name:'rohan',age:19}
+  ]);
+  const[fullname,setfullname]=useState('');
+  const [age ,setage]=useState(18);
 
-  const ChangeHandler =() =>{
-    setUsername("prahlad chouhan");
-  }
-   console.log(username);
+  const submitHandler=(e)=>{
+    e.preventDefault();
+    const newuser={fullname,age};
+    console.log(newuser);
+  };
+
+  const renderuser=username.map((user,index)=>{
+    return <li key={index}>{user.name}</li>
+  });
 
   return (
     <div>
-      <h1>UserName</h1>
-      <h2>{username}</h2>
-      <button onClick={ChangeHandler}>change name</button>
+      <h1>Register User</h1>
+      <form onSubmit={submitHandler}>
+    <input onChange={(e)=>setfullname(e.target.value)}
+    value={fullname}
+      type='text'
+      placeholder='Fullname'/>
+      <input onChange={(e)=>setage(e.target.value)}
+      value={age}
+      type='number'
+      placeholder='fullName' />
+      <button>submit</button>
+
+      <h1>User Data</h1>
+      <ol>{renderuser}</ol>
+      </form>
     </div>
   )
 }
