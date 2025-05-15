973115e16379a10fc977363c01c9bd87e100b1c8
PrahaldChouhan <prahladchouhan928@gmail.com>
2025-05-15 16:05:26 +0530

    Day-76 class react CURD opr

--- React-one/src/App.jsx
+++ React-one/src/App.jsx
@@ -1,24 +1,63 @@
+import { useState } from "react";
 
-import { useState } from 'react';
-import Create from './Components/Create';
-import Render from './Components/Render';
 
-function App() {
-  const [username,setUsername]=useState([
-    {name:'john',age:19},
-    {name:'ankur',age:32},
-    {name:'rohan',age:19}
-  ]);
+const App = () => {
+    const [todos, settodos] = useState([
+        { id: 1, title: "Kamm krle bhai", isCompleted: false },
 
-  return (
-    <div>
-      
-      <Create/>
-      <hr/>
-      <Render username={username} setUsername={setUsername}></Render>
-      
-    </div>
-  )
-}
 
+    ]);
+
+    const [title, settitle] = useState("");
+    const [completed, setcompleted] = useState(false);
+    const [gender, setgender] = useState("female");
+    const [city, setcity] = useState("delhi");
+
+    return (
+        <div>
+            <h1>Create Tasks</h1>
+            <form>
+                <input
+                    onChange={(e) => settitle(e.target.value)}
+                    value={title}
+                    type="text"
+                    placeholder="title"
+                />
+                <br />
+                <br />
+                <input
+                    checked={completed}
+                    onChange={(e) => setcompleted(e.target.checked)}
+                    type="checkbox"
+                />
+                Completed
+                <br /> <br />
+                <input
+                    value="male"
+                    onChange={(e) => setgender(e.target.value)}
+                    checked={gender == "male" && true}
+                    type="radio"
+                />
+                male
+                <input
+                    value="female"
+                    onChange={(e) => setgender(e.target.value)}
+                    checked={gender == "female" && true}
+                    type="radio"
+                />
+                Female
+                <br />
+                <br />
+                <select value={city} onChange={(e) => setcity(e.target.value)}>
+                    <option value="delhi">Delhi</option>
+                    <option value="mumbai">Mumbai</option>
+                    <option value="kolkata">Kolkata</option>
+                </select>
+                <br />
+                <br />
+                <button>Create Todo</button>
+            </form>
+        </div>
+    );
+};
 export default App;
